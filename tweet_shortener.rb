758141e8211ec0c2b7a982efb22863d96bc1dc5a
Ay Has <ahashim983@yahoo.com>
2019-03-31 23:38:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,7 +18,7 @@ def word_substituter(tweet)
   t = tweet.split(" ")
   for item in t
     if dictionary.keys.to_s.include?(item)
-      print "yes\n"
+      print dictionary.key_at(item)
     end
   end
 end
