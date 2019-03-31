f8fcb80f09b62d5020154f7b9506390dc6bfc322
Ay Has <ahashim983@yahoo.com>
2019-03-31 23:51:23 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,18 +1,17 @@
 def dictionary
   dictionary ={
-  :hello => "hi",
-  :to => "2",
-  :two => "2",
-  :too => "2",
-  :for => "4",
-  :four => "4",
-  :be => "b",
-  :you => "u",
-  :at => "@",
+  "hello"=> "hi",
+  "to" => "2",
+  "two"=> "2",
+  "too"=> "2",
+  "for"=> "4",
+  "four"=> "4",
+  "be"=> "b",
+  "you"=> "u",
+  "at"=> "@",
+  "and"=> "&",
 }
 end
-
-dictionary.fetch("be")
 
 def word_substituter(tweet)
   shortened = []
